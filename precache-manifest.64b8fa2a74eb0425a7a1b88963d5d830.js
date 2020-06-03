5bf5662519dcf921f12f33db676cb65f96e54438
Rawanul Momen <rusaf93@gmail.com>
2020-06-04 05:19:18 +0600

    Updates

---
+++ precache-manifest.64b8fa2a74eb0425a7a1b88963d5d830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c0f0f77d5eb1f26bd1051f54874253",
+    "url": "/monsters-search-react/index.html"
+  },
+  {
+    "revision": "5c680fa2d3171f5c5075",
+    "url": "/monsters-search-react/static/css/main.f7c00d75.chunk.css"
+  },
+  {
+    "revision": "c6069a82ddd5a7d3bb7a",
+    "url": "/monsters-search-react/static/js/2.5cca9c52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-search-react/static/js/2.5cca9c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c680fa2d3171f5c5075",
+    "url": "/monsters-search-react/static/js/main.f6f8d759.chunk.js"
+  },
+  {
+    "revision": "57c3c2b914ee2c608625",
+    "url": "/monsters-search-react/static/js/runtime-main.95e2f1ef.js"
+  }
+]);
